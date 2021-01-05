3c79be6b34a6b39198c283a9659c848b5617045a
Francine Guimarães <francinesguimaraes@outlook.com>
2021-01-05 15:31:07 -0300

    Project created

---
+++ backend/src/main/java/com/enic/ondelivery/OndeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.enic.ondelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OndeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OndeliveryApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/enic/ondelivery/OndeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enic.ondelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OndeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
