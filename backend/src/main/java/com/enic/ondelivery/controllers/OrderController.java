69e2c7f657d634530240653a8f41871f642cf28e
Francine Guimarães <francinesguimaraes@outlook.com>
2021-01-06 15:31:15 -0300

    Create endpoint POST /orders

--- backend/src/main/java/com/enic/ondelivery/controllers/OrderController.java
+++ backend/src/main/java/com/enic/ondelivery/controllers/OrderController.java
@@ -1,12 +1,16 @@
 package com.enic.ondelivery.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.enic.ondelivery.dto.OrderDTO;
 import com.enic.ondelivery.services.OrderService;
@@ -22,5 +26,13 @@ public class OrderController {
 	public ResponseEntity<List<OrderDTO>> find(){ //Special type that encapsulates a response HTTP
 		List<OrderDTO> list = service.find();
 		return ResponseEntity.ok().body(list); //ok says when the requisition was successfully made [ok code HTTP is 200]
+	}
+	
+	@PostMapping
+	public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto){
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto); //Returns a 201 code, witch is the right one when you make a insert
 	}
 }
--- backend/src/main/java/com/enic/ondelivery/services/OrderService.java
+++ backend/src/main/java/com/enic/ondelivery/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.enic.ondelivery.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.enic.ondelivery.dto.OrderDTO;
+import com.enic.ondelivery.dto.ProductDTO;
 import com.enic.ondelivery.entities.Order;
+import com.enic.ondelivery.entities.OrderStatus;
+import com.enic.ondelivery.entities.Product;
 import com.enic.ondelivery.repositories.OrderRepository;
+import com.enic.ondelivery.repositories.ProductRepository;
 
 @Service //Makes the class a registered component that can be injected in other components
 public class OrderService {
@@ -17,9 +22,24 @@ public class OrderService {
 	@Autowired //SpringBoot resource to make a dependency injection automatically
 	private OrderRepository repository;
 	
-	@Transactional(readOnly=true)
+	@Autowired
+	private ProductRepository productRepository;
+	
+	@Transactional(readOnly=true) //Just read cause it's just searching
 	public List<OrderDTO> find(){
 		List<Order> list = repository.findOrderWithProducts();
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList()); //Lambda expression
+	}
+	
+	@Transactional(readOnly=false)
+	public OrderDTO insert(OrderDTO dto){ //dto contains the order and its data
+		Order order = new Order(null, dto.getLatitude(), 
+				dto.getLongitude(), Instant.now(), dto.getAddress(), OrderStatus.PENDING);
+		for(ProductDTO p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId()); //Instance a new product based on p's ID
+			order.getProducts().add(product); //Add this new product on the order list, so now the product is associated to the order
+		}
+		order = repository.save(order);
+		return new OrderDTO(order);
 	}
 }
