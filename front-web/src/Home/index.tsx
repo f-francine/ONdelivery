2d567f4624149183cb1754e2262517e1a9c5fcbd
Francine Guimarães <francinesguimaraes@outlook.com>
2021-01-10 01:03:35 -0300

    Add Footer

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,30 +1,34 @@
 import './styles.css';
-import   { ReactComponent as ImgContent } from './img-content.svg';
+import { ReactComponent as ImgContent } from './img-content.svg';
+import Footer from '../Footer/index'
 
-function Home () {
+function Home() {
     return (
-        <div className="home-conteiner">
-            <div className="home-content">
-                <div className="home-actions">
-                    <p className="home-title-1">
-                        CASH ON
-                    </p>
-                    <p className="home-title-2">
-                        DELIVERY
-                    </p>
-                    <h1 className="home-subtitle">
-                        Faça seu pedido e<br /> o entregaremos até 
+        <>
+            <div className="home-container">
+                <div className="home-content">
+                    <div className="home-actions">
+                        <p className="home-title-1">
+                            CASH ON
+                        </p>
+                        <p className="home-title-2">
+                            DELIVERY
+                        </p>
+                        <h1 className="home-subtitle">
+                            Faça seu pedido e<br /> o levaremos até
                         você
-                    </h1>
-                    <a href="btn" className="home-btn-order">
-                        FAZER PEDIDO
-                    </a>
-                </div>
-                <div className="home-img">
-                    <ImgContent />
+                        </h1>
+                        <a href="btn" className="home-btn-order">
+                            FAZER PEDIDO
+                        </a>
+                    </div>
+                    <div className="home-img">
+                        <ImgContent />
+                    </div>
                 </div>
             </div>
-        </div>
+           <Footer />
+        </>
     )
 }
 
